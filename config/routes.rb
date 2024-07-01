c546f8e9a5b616720de8ca2a22512967b6771d6d
EC2 Default User <ec2-user@ip-172-31-43-140.ap-northeast-1.compute.internal>
2024-07-01 07:36:40 +0000

    finish bookers1-debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'#1）追加しました
   resources :books
+
 end
---
+++ db/migrate/20240701054837_add_title_to_table_name.rb
@@ -0,0 +1,6 @@
+class AddTitleToTableName < ActiveRecord::Migration[6.1]
+  def change
+    add_column :books, :title, :string
+  end
+end
+
